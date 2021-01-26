7b1727f82a8316d0c1b692a963779c61efb8d9b8
Marcelo Garoze <marcelogaroze@hotmail.com>
2021-01-25 21:45:51 -0300

    try: fix loading time for greete message

--- src/script.js
+++ src/script.js
@@ -1,39 +1,27 @@
-function startTime() { // days
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    const days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    m = checkTime(m);
-    s = checkTime(s);
-
-    var greet;
-    if (h < 12)
-        greet = 'Bom Dia ';
-    else if (h >= 12 && h <= 17)
-        greet = 'Boa Tarde ';
-    else if (h >= 17 && h <= 24)
-        greet = 'Boa Noite ';
-
-    //document.getElementById('sGreetings').innerHTML = `
-    //<b>${greet}</b> Garoze, hoje é <b>${days[today.getDay()]}-feira</b>!
-    //`
-
-    document.getElementById('sGreetings').innerHTML = `
-        <b>${greet}</b> Garoze <br> Hoje é <b>${days[today.getDay()]}-feira</b>!
-    `
-}
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};
+const checkTime = (i) => {
+    if (i < 10) return {i = "0" + i};
     return i;
 }
 
-// duckduck go
-function duckSearch() {
-    var searchField = document.getElementById("searchField");
-    if (searchField && searchField.value) {
-        var query = escape(searchField.value);
-        window.location.href = "https://duckduckgo.com/?q=" + query;
-    }
+const getGreetMessage = (actualHour) => {
+    if (actualHour < 12) return 'Bom Dia ';
+    if (actualHour >= 12 && actualHour <= 17) return 'Bom Tarde ';
+    if (actualHour >= 17 && actualHour <= 24) return 'Boa Noite ';
 }
+
+const startTime = () => {
+    const today = new Date();
+    const hour = today.getHours();
+    const dayWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+
+    document.getElementById('sGreetings').innerHTML = `
+        <b>${getGreetMessage(hour)}</b> Garoze <br> Hoje é <b>${dayWeek[today.getDay()]}-feira</b>!
+    `
+}
+
